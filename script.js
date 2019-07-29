d03a7a1c2b3e83e34afa6c8cc586362f786f4b87
horacio gutierrez <horacio.estevez@gmail.com>
2019-07-29 10:40:28 -0300

    termino ejemplo de historial

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let main = document.querySelector("main")
 links.forEach(link=>{
     link.addEventListener("click",e=>{
         e.preventDefault()
+        //URI
+        //location.hash = e.target.innerText
         history.pushState("","",e.target.innerText)
         ajax("get",e.target.href,render)
     })
@@ -26,6 +28,11 @@ function render(data){
     main.innerHTML = data
 }
 
+
+//popstate : se dispara cuando el usuario hace click en los botones de control de historial(atras o adelante), alt+<-/-> , history.{back|forward|go}()
 window.addEventListener("popstate",()=>{
-    
+    console.log("Cambio El historial")
+    //let url = location.pathname.split("/")[1] + ".html"
+    let url = location.pathname.substr(1) + ".html"
+    ajax("get",url,render)
 })
